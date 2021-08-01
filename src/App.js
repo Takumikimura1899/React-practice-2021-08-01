073c96b30b8cd0429e9ef25907677ea74d39b7d8
Takumikimura141899 <kimura141899@gmail.com>
2021-08-02 04:36:41 +0900

    コンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Router from './router/Router';
 
 import './App.css';
-import Home from './Home';
-import Page1 from './Page1';
-import Page1DetailA from './Page1DetailA';
-import Page1DetailB from './Page1DetailB';
-import Page2 from './Page2';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className='App'>
         <h1>Hello</h1>
         <Link to='/'>Home</Link>
@@ -17,34 +13,9 @@ function App() {
         <Link to='/page1'>Page1</Link>
         <br />
         <Link to='/page2'>Page2</Link>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route
-            path='/page1'
-            // 引数で渡されるpropsにはルーティングに関する様々なデータが入っている。
-            // その中のmatchの中のurlを使用する
-            render={({ match: { url } }) => (
-              <Switch>
-                <Route exact path={url}>
-                  <Page1 />
-                </Route>
-                <Route path={`${url}/detailA`}>
-                  <Page1DetailA />
-                </Route>
-                <Route path={`${url}/detailB`}>
-                  <Page1DetailB />
-                </Route>
-              </Switch>
-            )}
-          />
-          <Route path='/page2'>
-            <Page2 />
-          </Route>
-        </Switch>
+        <Router />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
