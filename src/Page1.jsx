6600795849bad28bd3b988cb25361d64c8aa8159
Takumikimura141899 <kimura141899@gmail.com>
2021-08-01 21:39:12 +0900

    ネストされたルーティング matchを使ったルーティング

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Page1 = () => {
   return (
     <div>
       <h1>Page1</h1>
+      <Link to='/page1/detailA'>DetailA</Link>
+      <br />
+      <Link to='/page1/detailB'>DetailB</Link>
     </div>
   );
 };
