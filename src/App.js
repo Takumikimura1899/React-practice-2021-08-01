6ee8c41a5ecca58d591e68162ac4b702ec0aba40
Takumikimura141899 <kimura141899@gmail.com>
2021-08-01 21:07:38 +0900

    通常のルーティング

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Home from './Home';
 import Page1 from './Page1';
@@ -5,12 +7,27 @@ import Page2 from './Page2';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-      <Home />
-      <Page1 />
-      <Page2 />
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Hello</h1>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/page1'>Page1</Link>
+        <br />
+        <Link to='/page2'>Page2</Link>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/page1'>
+            <Page1 />
+          </Route>
+          <Route path='/page2'>
+            <Page2 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
