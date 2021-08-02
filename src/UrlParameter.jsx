33c6e2b6b162850798a57aa4a036543071f35350
Takumikimura141899 <kimura141899@gmail.com>
2021-08-03 05:59:31 +0900

    クエリパラメータ

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 const UrlParameter = () => {
   const { id } = useParams();
+  // const location = useLocation();
+  // console.log(location);
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
   return (
     <div>
       <h1>UrlParameter</h1>
       <p>{id}</p>
+      <p>{query.get('name')}</p>
     </div>
   );
 };
